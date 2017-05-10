573d62079a3bf401030dd0f9ec41ead1e13ab51d
MonZta123 <datmonzta@gmx.at>
2017-05-11 01:12:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CGSharpJoin
+
+Simple Program to join multiple files to one big file for contests'n'stuff
+
+i recommend to change the output in the format you prefer
+feel free to share or modify or whatever
---
+++ SharpJoin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SharpJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output;
+            List<string[]> fileZ = Directory.GetFiles(".").Where(n => n.LastIndexOf(".cs") == n.Length - 3).Select(n => File.ReadAllLines(n)).ToList();
+            string[] usings = fileZ.ToList().SelectMany(n => n.Where(m => m.StartsWith("using"))).Distinct().ToArray();
+
+            output = String.Join("\n", usings) + "\n";
+            fileZ = fileZ.Select(m => m.Where(o => !o.StartsWith("using")).ToArray()).ToList();
+            fileZ.ToList().ForEach(n => output += String.Join("\n", n) + "\n");
+
+            Console.WriteLine(output);
+        }
+    }
+}
